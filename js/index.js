659672308e6d6fe35d624db80ce3bfd098b38cad
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-10-17 19:36:58 +0200

    Merging the merge

--- js/index.js
+++ js/index.js
@@ -2,6 +2,22 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+    // console.log(response);
+    // console.log(typeof response);
+    const data = await response.json();
+    // console.log(data);
+    // console.log(typeof data);
 
+    const foundItem = data.results.find((element) => element.name === "R2-D2");
+    // console.log(foundItem)
+    const eyeColor = foundItem.eye_color;
+    console.log(eyeColor);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 fetchData();
